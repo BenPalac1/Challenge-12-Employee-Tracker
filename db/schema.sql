30369490f13f5e2710b115d36f5486d1f8277d69
BenPalac1 <ben.palacpac@gmail.com>
2024-01-13 09:35:53 -0500

    updated schema and seeds, tested in mysql workbench, db sources, next seeds

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,7 @@ USE employee_db;
 
 CREATE TABLE departments (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    department_name VARCHAR(255) NOT NULL 
+    department_name VARCHAR(255) 
 );
 
 CREATE TABLE roles (
@@ -13,15 +13,17 @@ CREATE TABLE roles (
     title VARCHAR(255),
     salary DECIMAL(10,2),
     department_id INT,
-    FOREIGN KEY (department_id)
-    REFERENCES departments(id)
+    FOREIGN KEY (department_id) REFERENCES departments(id)
     ON DELETE SET NULL
 );
 
 CREATE TABLE employees (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
     role_id INT,
-    manager_id INT NOT NULL
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    manager_id INT,
+    FOREIGN KEY (manager_id) REFERENCES employees(id)  
+    ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -4,7 +4,7 @@ VALUES
 ('Backroom'),
 ('Guitars'),
 ('Pro Audio and Pianos'),
-('Drums and Percussion'),
+('Drums and Percussion');
 
 
 INSERT INTO roles (title, salary, department_id)
@@ -15,7 +15,7 @@ VALUES
 ('Guitar Sales Rep', 36500.00, 3),
 ('Drum Tech', 36500.00, 5),
 ('Piano Sales Rep', 36500.00, 4),
-('Pro Audio Sales Rep', 45000.00, 4),
+('Pro Audio Sales Rep', 45000.00, 4);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES
@@ -29,4 +29,4 @@ VALUES
 ('Lisa', 'Codrington', 5, 5),
 ('Bryan', 'Carroll', 6, 4),
 ('Aaron', 'Marshall', 7, 4),
-('Plini', 'Holgate', 7, 4),
+('Plini', 'Holgate', 7, 4);
