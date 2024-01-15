ac35b81ec9fc90350cff423d61af95db9b05f7f8
BenPalac1 <ben.palacpac@gmail.com>
2024-01-15 12:41:12 -0500

    updated schema.sql as per the mock up in the image in the module

--- db/schema.sql
+++ db/schema.sql
@@ -1,29 +1,34 @@
+-- create database and use
 DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 
 USE employee_db;
 
-CREATE TABLE departments (
+-- creat department table
+CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    department_name VARCHAR(255) 
+    name VARCHAR(30) 
 );
 
-CREATE TABLE roles (
+-- create role table
+CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255),
-    salary DECIMAL(10,2),
+    title VARCHAR(30),
+    salary DECIMAL,
     department_id INT,
     FOREIGN KEY (department_id) REFERENCES departments(id)
     ON DELETE SET NULL
 );
 
-CREATE TABLE employees (
+-- create employee table
+CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    FOREIGN KEY (role_id) REFERENCES roles(id),
+    FOREIGN KEY (role_id) REFERENCES role(id)
+    ON DELETE SET NULL
     manager_id INT,
-    FOREIGN KEY (manager_id) REFERENCES employees(id)  
+    FOREIGN KEY (manager_id) REFERENCES employee(id)  
     ON DELETE SET NULL
 );
