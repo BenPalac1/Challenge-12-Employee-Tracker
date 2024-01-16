63b8df56008533f0d6159ee3c06b58e0c4d1ef92
BenPalac1 <ben.palacpac@gmail.com>
2024-01-15 20:32:34 -0500

    fixed a mysql syntax error

--- db/schema.sql
+++ db/schema.sql
@@ -16,7 +16,7 @@ CREATE TABLE role (
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT,
-    FOREIGN KEY (department_id) REFERENCES departments(id)
+    FOREIGN KEY (department_id) REFERENCES department(id)
     ON DELETE SET NULL
 );
 
@@ -26,8 +26,7 @@ CREATE TABLE employee (
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    FOREIGN KEY (role_id) REFERENCES role(id)
-    ON DELETE SET NULL
+    FOREIGN KEY (role_id) REFERENCES role(id),
     manager_id INT,
     FOREIGN KEY (manager_id) REFERENCES employee(id)  
     ON DELETE SET NULL
