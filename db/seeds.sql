3c753287c4ce85677fbdeb7344a6416fc3df0a2f
BenPalac1 <ben.palacpac@gmail.com>
2024-01-15 20:33:01 -0500

    updated seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,18 +1,18 @@
-INSERT INTO departments (department_name)
+INSERT INTO department (name)
 VALUES
 ('Front of House'),
-('Backroom'),
+('Backroom');
 
-INSERT INTO roles (title, salary, department_id)
+INSERT INTO role (title, salary, department_id)
 VALUES
 ('Store Manager', 78000.00, 1),
 ('Inventory Manager', 65000.00, 2),
 ('Sales Manager', 55000.00, 1),
 ('Guitar Sales Rep', 36500.00, 1),
 ('Guitar Merch', 36500.00, 2),
-('Piano Sales Rep', 36500.00, 1),
+('Piano Sales Rep', 36500.00, 1);
 
-INSERT INTO employees (first_name, last_name, role_id, manager_id)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
 ('Jared', 'Keeso', 1, NULL),
 ('Nathan', 'Dales', 3, NULL),
